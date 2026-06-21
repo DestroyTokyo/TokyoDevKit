e36c6fc4411093704e43466290f150456b8f8817
Nionim (C!tory¿) <delta.cion.fe@gmail.com>
2026-06-22 01:21:06 +0300

    Update Constants.java

--- src/main/java/delta/cion/cherry/modKit/util/Constants.java
+++ src/main/java/delta/cion/cherry/modKit/util/Constants.java
@@ -27,7 +27,7 @@ public class Constants {
 	// CherryVersion is interface bruh
 	// Also I can cache it but idk..
 	// Nah, also I can create json mirror with version like maven but my own
-	public static final String CHERRY_VERSIONS_URL = "https://api.github.com/repos/CherryServer/CherryServer/releases";
+	public static final String CHERRY_VERSIONS_URL = "https://api.github.com/repos/DestroyTokyo/CherryServer/releases";
 
 	public static final String GRADLE_VERSIONS_URL = "https://services.gradle.org/versions/all";
 	public static final String SHADOW_PLUGIN_METADATA_URL = "https://plugins.gradle.org/m2/com/gradleup/shadow/com.gradleup.shadow.gradle.plugin/maven-metadata.xml";
